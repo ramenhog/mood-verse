6e93488c143fd79e40c8c9864c5519ee328152df
Stephanie Liu <sliu@rjmetrics.com>
2015-11-30 19:17:21 -0500

    random array of verses

--- script.js
+++ script.js
@@ -4,14 +4,33 @@ document.addEventListener("DOMContentLoaded", function() {
 
 var verseCallback = function() {
     var passage = "John+3:16-17";
+
+    function randomPassage() {
+        var verses = [
+            'John 3:16-17',
+            'Romans 8:6',
+            '1 Peter 2:9',
+            '2 Corinthians 5:9-10',
+            'Ephesians 6:16-18',
+            '1 Corinthians 13:1-3'
+        ];
+        $.each(verses, function(index, value){
+            verses[index] = value.replace(' ','+');
+        });
+        var i = Math.floor((Math.random() * verses.length));
+        console.log(verses.length);
+        console.log(i);
+        return verses[i];
+    }
+
+    var passage = randomPassage();
+
     var queryUrl = 'http://www.esvapi.org/v2/rest/passageQuery?key=IP&passage=' + passage + '&include-headings=false&include-footnotes=false&include-audio-link=false&include-short-copyright=false&output-format=plain-text';
     var randomUrl = 'http://www.esvapi.org/v2/rest/verse?key=IP&include-headings=false&include-footnotes=false&include-audio-link=false&include-short-copyright=false&output-format=plain-text';
 
     function format(data) {
         var verse = '';
-
         data = data.replace('=======================================================','').replace(/_______________________________________________________/g, '').split('[');
-        console.log(data);
 
         $('cite').html(data[0]);
 
@@ -19,17 +38,44 @@ var verseCallback = function() {
             if (i > 0) {
                 var line = data[i].split(']');
                 verse += line[1];
-                console.log(line[1]);
             }
         });
         return verse;
     }
 
     $.ajax({
-        url: randomUrl,  
+        url: queryUrl,  
         success:function(data) {
             data = format(data);
             $('#scripture').html(data);
         }
     });
+
+    function changeColor(){
+        var num = Math.floor((Math.random() * 5) + 1);
+        var color;
+        switch(num) {
+            case 1:
+                color = 'pomegranate';
+                break;
+            case 2:
+                color = 'yellow';
+                break;
+            case 3:
+                color = 'cerulean';
+                break;
+            case 4:
+                color = 'limeade';
+                break;
+            case 5:
+                color = 'downriver';
+                break;
+        }
+        $('body').addClass(color);
+    }
+    changeColor();
 };
+
+$('body').click(function(){
+    verseCallback();
+});
