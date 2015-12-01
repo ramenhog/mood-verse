4c70e1545ad44b67840062c21328d823e096e99a
Stephanie Liu <sliu@rjmetrics.com>
2015-12-01 17:36:22 -0500

    get moods to trigger verses

--- script.js
+++ script.js
@@ -5,41 +5,74 @@ document.addEventListener("DOMContentLoaded", function() {
 var verseCallback = function() {
     var apiKey = 'Nism4wChMEVRVUobXFGxS3UWGtDUP2gKRe8xVq6z';
     var passage = "John+3:16-17";
-    var verses = [];
+    var categories = [
+        'peace',
+        'love',
+        'wisdom',
+        'doubt',
+        'salvation',
+        'christmas'
+    ];
+    var allVerses = [];
+    var cat = categories[Math.floor(Math.random() * categories.length)];
 
-    function findTaggedRef(data) {
-        var references = data.response.tags[0].references;
-        $.each(references, function(index, val) {
-            verses.push({
-                start: references[index].reference.start/*,
-                end: references[index].reference.end*/
-            });
+    findMoodVerses();
+    getMood();
+
+    function getMood() {
+        $('.moods').find('li').on('click', function(e){
+            cat = $(this).attr('id');
+            e.stopPropagation();
+
+            findMoodVerses();
         });
-        console.log(verses[0].start.replace('GNT:',''));
-        passage = verses[0].start.replace('GNT:','');
-        findPassages(passage);
     }
-
-    function findPassages(passage){
+    // Find references from 
+    function findMoodVerses() {
+        // Uses http://bibles.org/pages/api/
         $.ajax({
-            url: 'https://bibles.org/v2/passages.js?q[]='+passage+'&version=eng-ESV',
+            url: 'https://bibles.org/v2/tags/'+cat+'.js',  
+            username: apiKey,
             dataType: 'json',
-            success: function(json) {
-                console.log(json.response.search.result.passages[0].text);
-                $('#scripture').html(json.response.search.result.passages[0].text);
+            success: function(data) {
+                var allReferences = data.response.tags[0].references;
+                var randomRef = allReferences[Math.floor(Math.random() * allReferences.length)];
+                var refStart = randomRef.reference.start;
+                var refEnd = randomRef.reference.end;
+                
+                var arr = refEnd.split('.'),
+                output = arr.pop();
+
+                refEnd = output;
+
+                var totalRef = refStart+'-'+output;
+                console.log('totalref:'+totalRef);
+                passage = totalRef.replace('GNT:','');
+                findPassages(totalRef);
             }
         });
     }
 
-    // Uses http://bibles.org/pages/api/
-    $.ajax({
-        url: "https://bibles.org/v2/tags/peace.js",  
-        username: apiKey,
-        dataType: 'json',
-        success: function(json) {
-            findTaggedRef(json);
-        }
-    });
+    function findPassages(passage){
+        console.log(passage);
+        $.ajax({
+            url: 'https://bibles.org/v2/passages.js?q[]='+passage+'&version=eng-ESV',
+            dataType: 'json',
+            success: function(json) {
+                var script = json.response.search.result.passages[0].text;
+                console.log(script);
+                formatPassage(script);
+            }
+        });
+    }
+
+    function formatPassage(passage){
+        var elem = $(passage);
+        elem.find('sup').remove().html;
+        var formatted = elem.html();
+        console.log(formatted);
+        $('#scripture').html(formatted);
+    }
 
     function changeColor(){
         var num = Math.floor((Math.random() * 5) + 1);
